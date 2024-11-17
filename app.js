fe6b0a9722838113ba52086f7fbd6ad0b01b5074
sophiefuerhoff <sophie.fuerhoff@gmx.net>
2024-11-17 23:08:28 +0100

    add max height footer

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let playbackDuration = 30; // Default playback duration
 let qrScanner;
 let csvCache = {};
 
-const versionNumber = "0.0.2";
+const versionNumber = "0.0.3";
 
 document.addEventListener('DOMContentLoaded', function () {
 
