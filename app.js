87ad71bfebcba7fad3de50abc9248012b1af4ae3
sophiefuerhoff <sophie.fuerhoff@gmx.net>
2024-11-25 22:02:23 +0100

    version++

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let playbackDuration = 30; // Default playback duration
 let qrScanner;
 let csvCache = {};
 
-const versionNumber = "0.0.4";
+const versionNumber = "0.0.5";
 
 document.addEventListener('DOMContentLoaded', function () {
 
